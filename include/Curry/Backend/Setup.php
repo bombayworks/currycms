a00dc236ed292661ed2a588a3e64e4e1e60b5c5a
Jesper Ek <deadbeef84@gmail.com>
2013-11-21 00:00:45 +0100

    Added missing docblock header to Curry_Backend_Setup.

--- include/Curry/Backend/Setup.php
+++ include/Curry/Backend/Setup.php
@@ -1,5 +1,26 @@
 <?php
+/**
+ * Curry CMS
+ *
+ * LICENSE
+ *
+ * This source file is subject to the GPL license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://currycms.com/license
+ *
+ * @category   Curry CMS
+ * @package    Curry
+ * @copyright  2011-2012 Bombayworks AB (http://bombayworks.se)
+ * @license    http://currycms.com/license GPL
+ * @link       http://currycms.com
+ */
 
+/**
+ * Curry setup/installation backend.
+ *
+ * @package Curry\Backend
+ */
 class Curry_Backend_Setup extends Curry_Backend {
 	
 	public static function getGroup()

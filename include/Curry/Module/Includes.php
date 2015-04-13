d3f3ccdd32ebaca53a5c6cd2aaa5e9cc7b151a07
Jesper Ek <deadbeef84@gmail.com>
2015-04-13 12:30:54 +0200

    Removed minify library/functionality.

--- include/Curry/Module/Includes.php
+++ include/Curry/Module/Includes.php
@@ -27,13 +27,6 @@ use Curry\Util\PathHelper;
  */
 class Includes extends AbstractModule {
 	/**
-	 * Minify JavaScript and CSS?
-	 *
-	 * @var bool
-	 */
-	protected $minify = false;
-	
-	/**
 	 * List of script files.
 	 *
 	 * @var array
@@ -65,45 +58,24 @@ class Includes extends AbstractModule {
 	}
 	
 	/** {@inheritdoc} */
-	public function getCacheProperties()
-	{
-		// If minification is enabled, cache for 10 minutes, otherwise we dont need to set a limit
-		return new CacheProperties(array(), $this->minify ? 600 : false);
-	}
-	
-	/** {@inheritdoc} */
 	public function showFront(\Curry_Twig_Template $template = null)
 	{
 		$pageGenerator = $this->app->generator;
 		if(!($pageGenerator instanceof HtmlGenerator))
-			throw new \Exception('Includes module only works on pages with PageGenerator set to Curry\Generator\Curry_PageGenerator_Html.');
+			throw new \Exception('Includes module only works on pages with PageGenerator set to Curry\Generator\HtmlGenerator.');
 		$head = $pageGenerator->getHtmlHead();
 		
 		// Stylesheets
-		$minify = array();
 		foreach($this->stylesheet as $stylesheet) {
-			if($this->minify && !$stylesheet['condition'] && strpos($stylesheet['source'], '//') === false) {
-				$minify[] = $stylesheet;
-				continue;
-			}
 			if($stylesheet['condition'])
 				$head->beginConditional($stylesheet['condition']);
 			$head->addStylesheet($stylesheet['source'], $stylesheet['media']);
 			if($stylesheet['condition'])
 				$head->endConditional();
 		}
-		if(count($minify)) {
-			$minifyUrl = $this->getMinifyCss($minify);
-			$head->addStylesheet($minifyUrl);
-		}
 		
 		// Scripts
-		$minify = array();
 		foreach($this->script as $script) {
-			if($this->minify && !$script['condition'] && strpos($script['source'], '://') === false) {
-				$minify[] = $script['source'];
-				continue;
-			}
 			if($script['condition'])
 				$head->beginConditional($script['condition']);
 			$attr = array();
@@ -115,151 +87,17 @@ class Includes extends AbstractModule {
 			if($script['condition'])
 				$head->endConditional();
 		}
-		if(count($minify)) {
-			$minifyUrl = $this->getMinifyJs($minify);
-			$head->addScript($minifyUrl, 'text/javascript');
-		}
 		
 		// Inline script
 		if($this->inlineScript)
 			$head->addInlineScript($this->inlineScript);
 	}
-	
-	/**
-	 * Get minified JavaScript path.
-	 *
-	 * @param array $files
-	 * @return string
-	 */
-	public function getMinifyJs($files)
-	{
-		$basedir = 'cache/';
-		$target = $basedir.sha1(serialize($files)).'.js';
-		
-		// create basedir if it doesnt exist
-		if(!is_dir($basedir))
-			@mkdir($basedir, 0777, true);
-		
-		// if minified file already exists, make sure it's up to date
-		if(file_exists($target)) {
-			$lastModified = self::getLastModified($files);
-			$targetModified = filemtime($target);
-			if($targetModified >= $lastModified) {
-				$this->app->logger->info('Using cached js minification file');
-				return $target . '?' . $targetModified;
-			}
-			$this->app->logger->notice('Updating js minification file');
-		}
-		
-		// Combine and minify files
-		$content = \Minify::combine($files);
-		
-		// Write content
-		file_put_contents($target, $content);
-		return $target . '?' . filemtime($target);
-	}
-	
-	/**
-	 * Get minified CSS path.
-	 *
-	 * @param array $files
-	 * @return string
-	 */
-	public function getMinifyCss($files)
-	{
-		$basedir = 'cache/';
-		$target = $basedir.sha1(serialize($files)).'.css';
-		
-		// create basedir if it doesnt exist
-		if(!is_dir($basedir))
-			@mkdir($basedir, 0777, true);
-		
-		// if minified file already exists, make sure it's up to date
-		if(file_exists($target)) {
-			$data = self::readMinificationHeader($target);
-			if(self::getLastModified($data['files']) == $data['modified']) {
-				$this->app->logger->info('Using cached css minification file');
-				return $target . '?' . $data['modified'];
-			}
-			$this->app->logger->notice('Updating css minification file');
-		}
-		
-		// Combine files (and process imports)
-		$imports = array();
-		$content = "";
-		foreach($files as $file) {
-			$processor = new CssProcessor($file['source']);
-			$relative = PathHelper::getRelative($basedir, dirname($file['source']));
-			$content .= $processor->getContent($file['media'], $relative);
-			$imports = array_merge($imports, $processor->getImportedFiles());
-		}
-		
-		// Minify
-		$source = new \Minify_Source(array(
-			'id' => $target,
-			'content' => $content,
-			'contentType' => \Minify::TYPE_CSS,
-		));
-		$content = \Minify::combine(array($source));
-		
-		// Add header
-		$header = array('files' => $imports, 'modified' => self::getLastModified($imports));
-		$content = "/* ".json_encode($header)." */\n" . $content;
-		
-		// Write content
-		file_put_contents($target, $content);
-		return $target . '?' . $header['modified'];
-	}
-	
-	/**
-	 * Reads minification header.
-	 * 
-	 * @param string
-	 * @return array
-	 */
-	protected static function readMinificationHeader($file)
-	{
-		$fp = @fopen($file, "r");
-		if(!$fp)
-			throw new \Exception('Unable to read minification target file: '.$file);
-		
-		$line = stream_get_line($fp, 4096, "\n");
-		fclose($fp);
-		
-		if(!preg_match('@/\* (.*) \*/$@', $line, $m))
-			throw new \Exception('Invalid minification target file: '.$file);
-			
-		$data = json_decode($m[1], true);
-		if(!$data)
-			throw new \Exception('Unable to parse minification file header: '.$file);
-			
-		return $data;
-	}
-	
-	/**
-	 * From an array of files, get the last modified time.
-	 *
-	 * @param array $files
-	 * @return int
-	 */
-	protected static function getLastModified(array $files)
-	{
-		$lastModified = 0;
-		foreach($files as $file)
-			$lastModified = max($lastModified, filemtime($file));
-		return $lastModified;
-	}
-	
+
 	/** {@inheritdoc} */
 	public function showBack()
 	{
 		$form = new \Curry_Form_SubForm(array(
-			'elements' => array(
-				'minify' => array('checkbox', array(
-					'label' => 'Minify',
-					'value' => $this->minify,
-				)),
-			)
+			'elements' => array()
 		));
 		
 		$scriptForm = new \Curry_Form_Dynamic(array(
@@ -268,7 +106,7 @@ class Includes extends AbstractModule {
 				'source' => array('filebrowser', array(
 					'label' => 'Source',
 					'required' => true,
-					'description' => 'May be a local or external file. However, currently external files are not minified.',
+					'description' => 'May be a local or external file.',
 				)),
 				'type' => array('text', array(
 					'label' => 'Type',
@@ -355,116 +193,8 @@ class Includes extends AbstractModule {
 	public function saveBack(\Zend_Form_SubForm $form)
 	{
 		$values = $form->getValues(true);
-		$this->minify = (bool)$values['minify'];
 		$this->script = (array)$values['script'];
 		$this->stylesheet = (array)$values['stylesheet'];
 		$this->inlineScript = $values['inline_script']['source'];
-	}
-}
-
-/**
- * @ignore
- */
-class CssProcessor {
-	protected $dir;
-	protected $rewrite;
-	protected $content;
-	protected $importedFiles = array();
-
-	public function __construct($path, array &$importedFiles = array())
-	{
-		$this->dir = dirname($path);
-		$content = (string)@file_get_contents($path);
-		
-		// keep track of imported files
-		$this->importedFiles =& $importedFiles;
-		$this->importedFiles[$path] = true;
-		
-		// remove UTF-8 BOM if present
-		if (pack("CCC",0xef,0xbb,0xbf) === substr($content, 0, 3))
-		$content = substr($content, 3);
-
-		// ensure uniform EOLs
-		$this->content = str_replace("\r\n", "\n", $content);
-	}
-
-	protected static function resolve($path, $parentDir)
-	{
-		$path = ($path{0} == '/' ? $path : $parentDir . '/' . $path);
-		$parts = array();
-		foreach(explode("/", $path) as $part) {
-			if($part == '..' && count($parts) && $parts[count($parts)-1] != '..')
-				array_pop($parts);
-			else if($part == '.' || $part == '')
-				continue;
-			else
-				array_push($parts, $part);
-		}
-		return join("/", $parts);
-	}
-
-	public function getContent($media = null, $rewrite = '')
-	{
-		$content = preg_replace_callback(
-			'/
-				@import\\s+
-				(?:url\\(\\s*)?      # maybe url(
-				[\'"]?               # maybe quote
-				(.*?)                # 1 = URI
-				[\'"]?               # maybe end quote
-				(?:\\s*\\))?         # maybe )
-				([a-zA-Z,\\s]*)?     # 2 = media list
-				;                    # end token
-			/x',
-			array($this, 'importCallback'),
-			$this->content
-		);
-
-		if($rewrite) {
-			$this->rewrite = $rewrite;
-			$content = preg_replace_callback(
-				'/url\\(\\s*([^\\)\\s]+)\\s*\\)/',
-				array($this, 'urlCallback'),
-				$content
-			);
-		}
-
-		return ($media && $media != 'all' ? "\n@media $media {\n$content\n}\n" : $content);
-	}
-
-	public function importCallback($m)
-	{
-		$url = $m[1];
-		$media = trim($m[2]);
-		
-		// external file, leave import in place
-		if (strpos($url, '//') !== false)
-			return $m[0];
-
-		// already imported?
-		$file = self::resolve($url, $this->dir);
-		if(array_key_exists($file, $this->importedFiles))
-			return '';
-
-		$p = new CssProcessor($file, $this->importedFiles);
-		return $p->getContent($media, dirname($url));
-	}
-
-	public function urlCallback($m)
-	{
-		$url = trim($m[1], '"\'');
-		if ($url[0] !== '/') {
-			if (strpos($url, '//') > 0) {
-				// skip externals
-			} else {
-				$url = self::resolve($url, $this->rewrite);
-			}
-		}
-		return "url('{$url}')";
-	}
-	
-	public function getImportedFiles()
-	{
-		return array_keys($this->importedFiles);
 	}
 }
